bb9b34c540560f7275b91311077ace952224cc1e
Christian Bianciotto <bianciottochristian@gmail.com>
2014-02-01 13:52:45 +0100

    add support to selecting visible galleries using comma-separated list of slug in dsframework-visible-gallery-categories custom field

--- ds-gallery-categories-navigation-template.php
+++ ds-gallery-categories-navigation-template.php
@@ -16,12 +16,19 @@
 		<?
 		$permalink = get_permalink();
 		$post_meta = get_post_custom();
+		$visible_gallery_categories = array();
 
 		$gallery_category_slug="";
 		if(isset($_GET) && isset($_GET['category'])) $gallery_category_slug=$_GET['category'];
 		if($gallery_category_slug=="") $gallery_category_slug = $post_meta['dsframework-root-gallery-category'][0];
+		if($gallery_category_slug=="" && isset($post_meta['dsframework-visible-gallery-categories'][0])) $visible_gallery_categories = explode(",", $post_meta['dsframework-visible-gallery-categories'][0]);
 
-		if($gallery_category_slug!=""){
+		if(count($visible_gallery_categories)>0){
+			foreach ( $visible_gallery_categories as $subgallery_category_slug ){
+				$category=get_gallery_category_by_slug( trim( $subgallery_category_slug) );
+				if($category!=null) $categories[]=$category;
+			}
+		}else if($gallery_category_slug!=""){
 			$gallery_category=get_gallery_category_by_slug($gallery_category_slug);
 
 			$categories = get_gallery_categories(array('hide_empty' => 0, 'parent' => $gallery_category->term_id));
@@ -39,7 +46,7 @@
 			$subcategory->navigation_link=add_query_arg( array('category' => $subcategory->slug), get_permalink());
 			$GLOBALS['category']=$subcategory;
 
-			if($subcategory->parent==0 || ($gallery_category_slug!="" && $subcategory->parent==$gallery_category->term_id)){
+			if($subcategory->parent==0 || ($gallery_category_slug!="" && $subcategory->parent==$gallery_category->term_id) || count($visible_gallery_categories)>0){
 				$postLoop = post_in_gallery_category( $subcategory, true );
 				if( $postLoop->have_posts() ) {
 					$postLoop->the_post();
